b93bed26fcc3e040e8748ae92ed7ed9339b78cb4
xiangcitoZ <pzxiang1@gmail.com>
2023-05-18 11:48:41 +0200

    Add project files.

---
+++ MvcCoreAWSS3/Controllers/AWSFilesController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreAWSS3.Services;
+
+namespace MvcCoreAWSS3.Controllers
+{
+    public class AWSFilesController : Controller
+    {
+        private ServiceStorageS3 service;
+
+        private string BucketUrl;
+        public AWSFilesController
+            (ServiceStorageS3 service, IConfiguration configuration)
+        {
+            this.BucketUrl = configuration.GetValue<string>("AWS:BucketUrl");
+            this.service = service;
+        }
+
+        public async Task<IActionResult> Index()
+        {   
+            List<string> filesS3 = await this.service.GetVersionsFileAsync();
+            ViewData["BUCKETURL"] = this.BucketUrl;
+            return View(filesS3);
+        }
+
+
+        public IActionResult UploadFile()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UploadFile(IFormFile file)
+        {
+            using(Stream stream = file.OpenReadStream())
+            {
+                await this.service.UploadFileAsync(file.FileName, stream);
+
+            }
+            return RedirectToAction("Index");
+        }
+
+        //SI DESEAMOS VISUALIZAR EL FICHERO POR SI FUERA PRIVADO
+        public async Task<IActionResult> FileAWS(string fileName)
+        {
+            Stream stream = await this.service.GetFileAsync(fileName);
+            return File(stream, "image/png");
+        }
+
+
+        public async Task<IActionResult> DeleteFile(string fileName)
+        {
+            await this.service.DeleteFileAsync(fileName);
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
---
+++ MvcCoreAWSS3/Services/ServiceStorageS3.cs
@@ -0,0 +1,81 @@
+﻿using Amazon.S3;
+using Amazon.S3.Model;
+
+namespace MvcCoreAWSS3.Services
+{
+    public class ServiceStorageS3
+    {
+        //EL NOMBRE DEL BUCKET LO INCLUIREMOS DENTRO DE 
+        //APPSETTINGS.JSON
+        private string BucketName;
+        //S3 TRABAJA CON UNA INTERFACE LLAMADA IAmazonS3
+        private IAmazonS3 ClientS3;
+        public ServiceStorageS3(IConfiguration configuration
+            , IAmazonS3 clientS3)
+        {
+            this.BucketName = configuration.GetValue<string>
+                ("AWS:BucketName");
+            this.ClientS3 = clientS3;
+        }
+
+        //COMENZAMOS SUBIENDO FICHEROS AL BUCKET
+        //NECESITAMOS FileName, Stream y un Key/Value
+        public async Task<bool>
+            UploadFileAsync(string fileName, Stream stream)
+        {
+            PutObjectRequest request = new PutObjectRequest
+            {
+                InputStream = stream,
+                Key = fileName,
+                BucketName = this.BucketName
+            };
+            //DEBEMOS OBTENER UNA RESPUESTA CON EL MISMO TIPO 
+            //DE REQUEST
+            PutObjectResponse response = await
+                this.ClientS3.PutObjectAsync(request);
+            if (response.HttpStatusCode == System.Net.HttpStatusCode.OK)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        //ELIMINAMOS FICHEROS DEL BUCKET
+        public async Task<bool> DeleteFileAsync(string fileName)
+        {
+            DeleteObjectResponse response =
+                await this.ClientS3.DeleteObjectAsync(this.BucketName, fileName);
+            if (response.HttpStatusCode == System.Net.HttpStatusCode.OK)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        //METODO PARA RECUPERAR LAS VERSIONES DE UN FILE
+        public async Task<List<string>> GetVersionsFileAsync()
+        {
+            ListVersionsResponse response =
+                await this.ClientS3.ListVersionsAsync(this.BucketName);
+            List<string> versiones =
+                response.Versions.Select(x => x.Key).ToList();
+            return versiones;
+        }
+
+        //METODO PARA RECUPERAR UN FILE POR CODIGO
+        public async Task<Stream> GetFileAsync(string fileName)
+        {
+            GetObjectResponse response =
+                await this.ClientS3.GetObjectAsync(this.BucketName, fileName);
+            return response.ResponseStream;
+        }
+
+    }
+
+}
